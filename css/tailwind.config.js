fabe13c0ad16812c9fa699cef9aca0891c4303f3
Monal-Patel <monal-@hotmail.com>
2022-12-13 13:00:29 +0000

    small changes

--- css/tailwind.config.js
+++ css/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["../../layouts/**/*.{html,js}","../../../../post/**/*.{md}"],
+  content: ["../../layouts/**/*.{html,js}","../../../../post/**/*.md"],
   theme: {
     extend: {},
     fontFamily: {'tufte': ['et-book','serif']
