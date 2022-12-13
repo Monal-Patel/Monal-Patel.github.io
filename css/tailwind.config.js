97fd9ac2e9754496e55f7239518968357ba25d4a
Monal-Patel <monal-@hotmail.com>
2022-12-13 02:35:39 +0000

    add sidebar

---
+++ css/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["../../layouts/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {'tufte': ['et-book','serif']
+    },
+  },
+  plugins: [],
+}
